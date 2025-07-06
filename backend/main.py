d137e9a9d46b862709c28b3175690453abfeb112
ArthurWalker <whitefang17996@gmail.com>
2025-07-06 22:19:13 +0100

    Flask server and basic react

--- backend/main.py
+++ backend/main.py
@@ -10,11 +10,12 @@ def get_contacts():
     json_contacts = [contact.to_json() for contact in contacts] # or list(map(lambda x: x.to_json(), contacts))
     return jsonify({"contacts":json_contacts})
 
-@app.route('/create_contacts',methods= ['POST'])
+@app.route('/create_contact',methods= ['POST'])
 def create_contacts():
     first_name = request.json.get('first_name')
     last_name = request.json.get('last_name')
     email = request.json.get('email')
+    print(first_name, last_name, email)
 
     if not first_name or not last_name or not email:
         return (jsonify({"message": "You must include a first name, last name and email"}),400,)
